b49e244a8f9e159814ba7bc718c74cff8108a78e
skskdmsdl <skskdmsdl@gmail.com>
2021-02-14 14:54:10 +0900

    movie정보 가져와서 출력

--- src/Movie.js
+++ src/Movie.js
@@ -2,8 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // movie컴포넌트는 state를 가지고 있지 않기 때문에 class 컴포넌트일 필요 없음(function 컴포넌트 사용)
-function Movie({id, year, title, summary, poster}){
-    return <h4>{title}</h4>;
+function Movie({year, title, summary, poster}){
+    return (
+        <div class="movies__movie">
+            <img src={poster} alt={title} title={title} />
+            <div class="movie__data">
+                <h3 class="movie__title">{title}</h3>
+                <h5 class="movie__year">{year}</h5>
+                <p class="movie__summary">{summary}</p>
+            </div>
+        </div>
+    );
 };
 
 Movie.prototype = {
