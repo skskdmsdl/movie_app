6cd3f28ef286b9edcff650b3fdf600cf46665a40
skskdmsdl <skskdmsdl@gmail.com>
2021-02-14 13:59:45 +0900

    Movie Database API 가져오기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// props가 내가 원하는 props인지 확인하기 위해 prop-types 설치 & import → npm i prop-types
+import PropTypes from "prop-types"; 
+// npm i axios를 통해 axios 설치
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  // async 비동기(기다려라)
+  getMovies = async() => {
+    // 함수 내부에 await 뭘 기다리길 원해? axios? 응(완료까지 시간이 좀 필요하기에 await사용)
+    // movies.data.data를 아래처럼 나타낼 수 있음
+    const { data: { data: { movies }}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // movies:movies를 movies로 해도 자바스크립트가 이해함
+    this.setState({ movies, isLoading: false });
+    
+    console.log(movies);
+  }
+  componentDidMount() {
+    this.getMovies();
+  };
+  render() {
+    const { isLoading } = this.state;
+    return <div>{this.state.isLoading ? "Loading" : "We are ready"}</div>;
+  };
 }
+  
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// <App />는 컴포넌트를 의미함
+// 컴포넌트는 HTML을 반환하는 함수
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
