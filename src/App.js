8c41d0e49c5139cef001213a3658f1fd95557ede
skskdmsdl <skskdmsdl@gmail.com>
2021-03-01 19:11:48 +0900

    navigation 추가

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App(){
   return (
+    // BrowserRouter를 사용하면 url에 #이 안붙지만 github pages에서 정확히 설정하기 귀찮음
     <HashRouter>
+      {/* 컴포넌트가 Link를 사용하고 있다면 Router안에 넣어야 함! */}
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
       {/* <Route path="/home">
         <h1>Home</h1>
       </Route>
@@ -15,8 +22,6 @@ function App(){
       <Route path="/about">
         <h1>About</h1>
       </Route> */}
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
     </HashRouter>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navigation(){
+    return (
+        <div>
+            {/* <a herf="/">Home</a> 를 사용하면 react의 spa의 장점을 못살림
+                react-router-dom을 사용하여 <Link to="/">Home</Link>로 사용하기 */}
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+        </div>
+    );
+}
+
+export default Navigation;
